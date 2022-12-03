0fdde90495a1411de3b75518a56baa16c3d925c3
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-02 23:55:30 -0600

    Update SQLliteConnection.cs

--- SQLliteConnection.cs
+++ SQLliteConnection.cs
@@ -24,7 +24,41 @@ namespace Calculator
             Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
     }
 
+    public class SQLliteConnection
+    {
+        SQLiteAsyncConnection Database;
+        
+        public void DatabaseConnection()
+        {
+            Init();
+        }
 
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await Database.CreateTableAsync<CalculationHistory>();
+        }
+
+        public async Task<List<CalculationHistory>> GetHistoryAsync()
+        {
+            await Init();
+            return await Database.Table<CalculationHistory>().ToListAsync();
+        }
+
+        public async Task<int> SaveItemAsync(CalculationHistory item)
+        {
+            await Init();
+            return await Database.InsertAsync(item);
+        }
+
+        public async Task<int> DeleteAllAsync()
+        {
+            await Init();
+            return await Database.DeleteAllAsync<CalculationHistory>();
+        }
     
     
     }
