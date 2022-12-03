db41dd9626840745fab3b0e6d6ab8031bb101613
Harshavardhanguttikonda <115022190+Harshavardhanguttikonda@users.noreply.github.com>
2022-12-02 21:05:47 -0600

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -25,5 +25,23 @@ public static class Calculator
         return result;
     }
 }
+public static class DoubleExtensions
+{
+    public static string ToTrimmedString(this double target, string decimalFormat)
+    {
+        string strValue = target.ToString(decimalFormat); //Get the stock string
 
+        //If there is a decimal point present
+        if (strValue.Contains("."))
+        {
+            //Remove all trailing zeros
+            strValue = strValue.TrimEnd('0');
 
+            //If all we are left with is a decimal point
+            if (strValue.EndsWith(".")) //then remove it
+                strValue = strValue.TrimEnd('.');
+        }
+
+        return strValue;
+    }
+}
