d15d49448746b71fdf548c68dc8d90beb587007b
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-02 23:31:40 -0600

    Add files via upload

--- HistoryViewModel.cs
+++ HistoryViewModel.cs
@@ -19,7 +19,6 @@ namespace Calculator
         private ObservableCollection<CalculationHistory> calcualtionHistory;
         private SQLliteConnection SqlConnection;
 
-
         public HistoryViewModel()
         {
             calcualtionHistory = new ObservableCollection<CalculationHistory>();
@@ -40,10 +39,31 @@ namespace Calculator
             OnPropertyChanged();
         }
 
+        public async Task Init()
+        {
+            var res = await SqlConnection.GetHistoryAsync();
+            calcualtionHistory = new ObservableCollection<CalculationHistory>();
+            res.ForEach(calcualtionHistory.Add);
+        }
+
+        public async Task clearDatabase()
+        {
+            calcualtionHistory = new ObservableCollection<CalculationHistory>();
+            OnPropertyChanged("history");
+            await SqlConnection.DeleteAllAsync();
+            await Init();
+            OnPropertyChanged("history");
+            OnPropertyChanged();
+            OnPropertyChanged();
+        }
 
 
 
+        public ObservableCollection<CalculationHistory> historyExpressions
+        {
+            get => calcualtionHistory;
+        }
 
-
-
+        public void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 # Calculator
 veerasekhar patibandla
-Initial commit ->150cfeab0195a750a0678cf85a54d92e1ad4888e
+Initial commit 
+created history view .cs
 Added Navigation->> 693b90b2556b7e36012d5787c13dc9be00f31d01
 Added pages->1b2142871a8b75bf5c5d788eacc61d9553d4698d
-merged pull request ->> dheeraj reddy3
+merged pull request ->> dheeraj reddy2
 merged pull request#3->dheeraj reddy2
+created historyview model.cs
 merged pull request#5-> harshavardhan 
 updated calculator.page.xaml.cs
 
 
 Dheeraj reddy badduri:
 Updated calculator page->26c705bfd3ccb2ce22a657f733beee473b71cef0
+updated history view .cs
+updated  historyview model.cs
+created history.cs
 updated About page
 merged pull request#2
 merged pull request#3
 
 Harshavardhan guttikonda:
 updated claculator page.xaml.cs
+updated history view .cs
 updated home page.xaml
 updated Homepage.xaml.cs
+updated history.cs
+created historyview model.cs
 
